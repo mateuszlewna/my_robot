4f605f742fbc421b8f2258b8afe9a037823f9d68
Mateusz Lewna <mateuszlewna0505@gmail.com>
2025-08-06 12:42:42 +0200

    coś tam poprawiamy

--- src/robot/launch/localization_launch.py
+++ src/robot/launch/localization_launch.py
@@ -25,7 +25,7 @@ from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('my_robot')
+    bringup_dir = get_package_share_directory('robot')
 
     namespace = LaunchConfiguration('namespace')
     map_yaml_file = LaunchConfiguration('map')
@@ -64,7 +64,7 @@ def generate_launch_description():
 
         DeclareLaunchArgument(
             'map',
-            default_value=os.path.join(bringup_dir, 'maps', 'mapa_protolabu.yaml'),
+            default_value=os.path.join(bringup_dir, 'maps', 'mapa_protolabu_map.yaml'),
             description='Full path to map yaml file to load'),
 
         DeclareLaunchArgument(
--- src/robot/launch/navigation_launch.py
+++ src/robot/launch/navigation_launch.py
@@ -25,7 +25,7 @@ from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('my_robot')
+    bringup_dir = get_package_share_directory('robot')
 
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
